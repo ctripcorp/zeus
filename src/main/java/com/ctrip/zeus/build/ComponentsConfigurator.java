5098b7f86c8ebfb303bec88c754d3d75467a2f60
xingchaowang <xingchaowang@ctrip.com>
2015-03-03 20:14:43 +0800

    add dependencies'

---
+++ src/main/java/com/ctrip/zeus/build/ComponentsConfigurator.java
@@ -0,0 +1,28 @@
+package com.ctrip.zeus.build;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.unidal.dal.jdbc.datasource.JdbcDataSourceDescriptorManager;
+import org.unidal.lookup.configuration.AbstractResourceConfigurator;
+import org.unidal.lookup.configuration.Component;
+
+public class ComponentsConfigurator extends AbstractResourceConfigurator {
+   @Override
+   public List<Component> defineComponents() {
+      List<Component> all = new ArrayList<Component>();
+
+      // move following line to top-level project if necessary
+		all.add(C(JdbcDataSourceDescriptorManager.class) //
+				.config(E("datasourceFile").value("datasources.xml"), //
+						E("baseDirRef").value("ZEUS_HOME")));
+
+      all.addAll(new ZeusDatabaseConfigurator().defineComponents());
+
+      return all;
+   }
+
+   public static void main(String[] args) {
+      generatePlexusComponentsXmlFile(new ComponentsConfigurator());
+   }
+}
