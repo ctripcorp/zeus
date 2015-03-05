1e546e7413882b810840e1fc0dff488b28368b64
xingchaowang <xingchaowang@ctrip.com>
2015-03-05 22:37:46 +0800

    day dev checkpoint

--- src/main/java/com/ctrip/zeus/build/GenerateDaoBeansConfig.java
+++ src/main/java/com/ctrip/zeus/build/GenerateDaoBeansConfig.java
@@ -1,5 +1,9 @@
 package com.ctrip.zeus.build;
 
+import org.unidal.dal.jdbc.AbstractDao;
+
+import java.util.List;
+
 /**
  * @author:xingchaowang
  * @date: 3/5/2015.
@@ -9,5 +13,10 @@ public class GenerateDaoBeansConfig {
     public static void main(String[] args) {
         String daoPackage="com.ctrip.zeus.dal.core";
 
+        List<Class<?>> list = ClassFinder.find(daoPackage);
+        for (Class<?> clazz : list) {
+            if(AbstractDao.class.isAssignableFrom(clazz))
+            System.out.println(clazz.getName());
+        }
     }
 }
