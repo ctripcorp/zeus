65e674eb5cbc3c9a62a6c0e456dc437301dddcc4
xingchaowang <xingchaowang@ctrip.com>
2015-03-11 16:13:46 +0800

    add NginxConf

--- src/main/java/com/ctrip/zeus/nginx/conf/HealthCheckConf.java
+++ src/main/java/com/ctrip/zeus/nginx/conf/HealthCheckConf.java
@@ -20,7 +20,7 @@ public class HealthCheckConf {
                 .append(" timeout=").append(1000)
                 .append(" type=http").append(";\n");
         b.append("    ").append("check_keepalive_requests 100").append(";\n");
-        b.append("    ").append("check_http_send \"GET ").append(h.getUri()).append(" HTTP/1.1\\r\\nConnection: keep-alive\\r\\n\\r\\n\"").append(";\n");
+        b.append("    ").append("check_http_send \"GET ").append(h.getUri()).append(" HTTP/1.0\\r\\nConnection: keep-alive\\r\\n\\r\\n\"").append(";\n");
         b.append("    ").append("check_http_expect_alive http_2xx http_3xx").append(";\n");
         return b.toString();
     }
---
+++ src/main/java/com/ctrip/zeus/nginx/conf/StatusConf.java
@@ -0,0 +1,29 @@
+package com.ctrip.zeus.nginx.conf;
+
+import com.netflix.config.DynamicIntProperty;
+import com.netflix.config.DynamicPropertyFactory;
+
+/**
+ * @author:xingchaowang
+ * @date: 3/11/2015.
+ */
+public class StatusConf {
+    private static DynamicIntProperty nginxStatusPort = DynamicPropertyFactory.getInstance().getIntProperty("slb.nginx.status-port", 10001);
+
+
+    public static String generate() {
+        StringBuilder b = new StringBuilder(128);
+        b.append("server {").append("\n");
+        b.append("    listen    ").append(String.valueOf(nginxStatusPort.get())).append(";\n");
+        b.append("    location / {").append("\n");
+
+        b.append("        check_status").append(";\n");
+
+        b.append("    }").append("\n");
+        b.append("}").append("\n");
+
+
+
+        return b.toString();
+    }
+}
