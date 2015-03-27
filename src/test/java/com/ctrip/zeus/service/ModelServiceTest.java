54173a0307a00b6c992b8c6fd5faf85e9ec56e9d
Mengyi Zhou <bjrara@sina.com>
2015-03-27 18:23:21 +0800

    update test

--- src/test/java/com/ctrip/zeus/service/ModelServiceTest.java
+++ src/test/java/com/ctrip/zeus/service/ModelServiceTest.java
@@ -1,9 +1,6 @@
 package com.ctrip.zeus.service;
 
-import com.ctrip.zeus.model.entity.App;
-import com.ctrip.zeus.model.entity.AppList;
-import com.ctrip.zeus.model.entity.Slb;
-import com.ctrip.zeus.model.entity.SlbList;
+import com.ctrip.zeus.model.entity.*;
 import com.ctrip.zeus.model.transform.DefaultJsonParser;
 import com.ctrip.zeus.service.model.AppRepository;
 import com.ctrip.zeus.service.model.SlbRepository;
@@ -20,6 +17,8 @@ import support.MysqlDbServer;
 import javax.annotation.Resource;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by zhoumy on 2015/3/24.
@@ -38,48 +37,25 @@ public class ModelServiceTest extends AbstractSpringTest {
     private long insertedTestAppId;
 
     @BeforeClass
-    public static void setup() throws ComponentLookupException, ComponentLifecycleException {
+    public static void setUpDb() throws ComponentLookupException, ComponentLifecycleException {
         S.setPropertyDefaultValue("CONF_DIR", new File("").getAbsolutePath() + "/conf/test");
         mysqlDbServer = new MysqlDbServer();
         mysqlDbServer.start();
     }
 
     @Before
-    public void setUp() {
+    public void fillDb() {
         addSlb();
         addApps();
     }
 
-    private void addSlb() {
-        String slbJsonData = "{\"name\": \"default\", \"version\": 1, \"nginx-bin\": \"/opt/app/nginx/sbin\", \"nginx-conf\": \"/opt/app/nginx/conf\", \"nginx-worker-processes\": 2, \"status\": \"TEST\", \"vips\": [{\"ip\": \"10.2.25.93\"} ], \"slb-servers\": [{\"ip\": \"10.2.25.93\", \"host-name\": \"uat0358\", \"enable\": true }, {\"ip\": \"10.2.25.94\", \"host-name\": \"uat0359\", \"enable\": true }, {\"ip\": \"10.2.25.95\", \"host-name\": \"uat0360\", \"enable\": true } ], \"virtual-servers\": [{\"name\": \"testsite1\", \"ssl\": false, \"port\": \"80\", \"domains\": [{\"name\": \"s1.ctrip.com\"} ] }, {\"name\": \"testsite2\", \"ssl\": false, \"port\": \"80\", \"domains\": [{\"name\": \"s2a.ctrip.com\"}, {\"name\": \"s2b.ctrip.com\"} ] } ] }";
-        defaultSlb = parseSlb(slbJsonData);
-        try {
-            slbRepo.add(defaultSlb);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    private void addApps() {
-        String appJsonData = "{\"name\": \"testApp\", \"app-id\": \"000000\", \"version\": 1, \"app-slbs\": [{\"slb-name\": \"default\", \"path\": \"/\", \"virtual-server\": {\"name\": \"testsite2\", \"ssl\": false, \"port\": \"80\", \"domains\": [{\"name\": \"tests2.ctrip.com\"} ] } } ], \"health-check\": {\"intervals\": 5000, \"fails\": 1, \"passes\": 1, \"uri\": \"/domaininfo/OnService.html\"}, \"load-balancing-method\": {\"type\": \"roundrobin\", \"value\": \"test\"}, \"app-servers\": [{\"port\": 8080, \"weight\": 1, \"max-fails\": 2, \"fail-timeout\": 30, \"ip\": \"10.2.6.201\"}, {\"port\": 8080, \"weight\": 2, \"max-fails\": 2, \"fail-timeout\": 30, \"ip\": \"10.2.6.202\"} ] }";
-        testApp = parseApp(appJsonData);
-        try {
-            insertedTestAppId = appRepo.add(testApp);
-            Assert.assertTrue(insertedTestAppId > 0);
-            for(int i = 0; i < 6; i++) {
-                String appJsonData1 = "{\"name\": \"testApp" + i + "\", \"app-id\": \"000000\", \"version\": 1, \"app-slbs\": [{\"slb-name\": \"default\", \"path\": \"/\", \"virtual-server\": {\"name\": \"testsite1\", \"ssl\": false, \"port\": \"80\", \"domains\": [{\"name\": \"tests1.ctrip.com\"} ] } } ], \"health-check\": {\"intervals\": 5000, \"fails\": 1, \"passes\": 1, \"uri\": \"/domaininfo/OnService.html\"}, \"load-balancing-method\": {\"type\": \"roundrobin\", \"value\": \"test\"}, \"app-servers\": [{\"port\": 8080, \"weight\": 1, \"max-fails\": 2, \"fail-timeout\": 30, \"ip\": \"10.2.6.201\"}, {\"port\": 8080, \"weight\": 2, \"max-fails\": 2, \"fail-timeout\": 30, \"ip\": \"10.2.6.202\"} ] }";
-                appRepo.add(parseApp(appJsonData1));
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
+    /********************* test SlbRepository *********************/
 
     @Test
     public void testListSlbs() {
         try {
-            SlbList list = slbRepo.list();
-            Assert.assertTrue(list.getSlbs().size() >= 1);
+            List<Slb> list = slbRepo.list();
+            Assert.assertTrue(list.size() >= 1);
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -90,6 +66,27 @@ public class ModelServiceTest extends AbstractSpringTest {
         try {
             Slb slb = slbRepo.get(defaultSlb.getName());
             assertSlbEquals(defaultSlb, slb);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void testGetSlbBySlbServer() {
+        try {
+            Slb slb = slbRepo.getBySlbServer(defaultSlb.getVips().get(0).getIp());
+            assertSlbEquals(defaultSlb, slb);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void testListSlbsByAppServerAndAppName() {
+        try {
+            List<Slb> slbs = slbRepo.listByAppServerAndAppName("10.2.6.201", "testApp");
+            Assert.assertEquals(1, slbs.size());
+            assertSlbEquals(defaultSlb, slbs.get(0));
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -108,6 +105,43 @@ public class ModelServiceTest extends AbstractSpringTest {
             e.printStackTrace();
         }
     }
+
+    @Test
+    public void testListAppServersBySlb() {
+        try {
+            List<String> appServers = slbRepo.listAppServersBySlb(defaultSlb.getName());
+            Assert.assertEquals(testApp.getAppServers().size(), appServers.size());
+
+            List<String> appServersRef = new ArrayList<>();
+            for(AppServer as : testApp.getAppServers()) {
+                appServersRef.add(as.getIp());
+            }
+            Assert.assertFalse(appServersRef.retainAll(appServers));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void addSlb() {
+        String slbJsonData = "{\"name\": \"default\", \"version\": 1, \"nginx-bin\": \"/opt/app/nginx/sbin\", \"nginx-conf\": \"/opt/app/nginx/conf\", \"nginx-worker-processes\": 2, \"status\": \"TEST\", \"vips\": [{\"ip\": \"10.2.25.93\"} ], \"slb-servers\": [{\"ip\": \"10.2.25.93\", \"host-name\": \"uat0358\", \"enable\": true }, {\"ip\": \"10.2.25.94\", \"host-name\": \"uat0359\", \"enable\": true }, {\"ip\": \"10.2.25.95\", \"host-name\": \"uat0360\", \"enable\": true } ], \"virtual-servers\": [{\"name\": \"testsite1\", \"ssl\": false, \"port\": \"80\", \"domains\": [{\"name\": \"s1.ctrip.com\"} ] }, {\"name\": \"testsite2\", \"ssl\": false, \"port\": \"80\", \"domains\": [{\"name\": \"s2a.ctrip.com\"}, {\"name\": \"s2b.ctrip.com\"} ] } ] }";
+        defaultSlb = parseSlb(slbJsonData);
+        try {
+            slbRepo.add(defaultSlb);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void deleteSlb() {
+        String slbName = "default";
+        try {
+            Assert.assertEquals(1, slbRepo.delete(slbName));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /********************* test AppRepository *********************/
 
     @Test
     public void testGetApp() {
@@ -132,8 +166,8 @@ public class ModelServiceTest extends AbstractSpringTest {
     @Test
     public void testListApps() {
         try {
-            AppList list = appRepo.list();
-            Assert.assertTrue(list.getApps().size() >= 7);
+            List<App> list = appRepo.list();
+            Assert.assertTrue(list.size() >= 7);
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -142,8 +176,8 @@ public class ModelServiceTest extends AbstractSpringTest {
     @Test
     public void testListLimitApps() {
         try {
-            AppList list = appRepo.listLimit(insertedTestAppId, 3);
-            Assert.assertTrue(list.getApps().size() == 3);
+            List<App> list = appRepo.listLimit(insertedTestAppId, 3);
+            Assert.assertTrue(list.size() == 3);
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -154,8 +188,8 @@ public class ModelServiceTest extends AbstractSpringTest {
         String slbName = "default";
         String virtualServerName = "testsite1";
         try {
-            AppList list = appRepo.list(slbName, virtualServerName);
-            Assert.assertTrue(list.getApps().size() >= 6);
+            List<App> list = appRepo.list(slbName, virtualServerName);
+            Assert.assertTrue(list.size() >= 6);
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -175,12 +209,49 @@ public class ModelServiceTest extends AbstractSpringTest {
         }
     }
 
-    @After
-    public void tearDown() {
-        deleteApps();
-        deleteSlb();
+    @Test
+    public void testListAppsByAppServer() {
+        try {
+            List<String> appNames = appRepo.listAppsByAppServer(testApp.getAppServers().get(0).getIp());
+            boolean containsTestApp = false;
+            for (String appName : appNames) {
+                if(appName.equals(testApp.getName()))
+                    containsTestApp = true;
+            }
+            Assert.assertTrue(containsTestApp);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
+    @Test
+    public void testListAppServersByApp() {
+        try {
+            List<String> appServers = appRepo.listAppServersByApp(testApp.getName());
+            List<String> appServersRef = new ArrayList<>();
+            for(AppServer as : testApp.getAppServers()) {
+                appServersRef.add(as.getIp());
+            }
+            Assert.assertFalse(appServersRef.retainAll(appServers));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void addApps() {
+        String appJsonData = "{\"name\": \"testApp\", \"app-id\": \"000000\", \"version\": 1, \"app-slbs\": [{\"slb-name\": \"default\", \"path\": \"/\", \"virtual-server\": {\"name\": \"testsite2\", \"ssl\": false, \"port\": \"80\", \"domains\": [{\"name\": \"tests2.ctrip.com\"} ] } } ], \"health-check\": {\"intervals\": 5000, \"fails\": 1, \"passes\": 1, \"uri\": \"/domaininfo/OnService.html\"}, \"load-balancing-method\": {\"type\": \"roundrobin\", \"value\": \"test\"}, \"app-servers\": [{\"port\": 8080, \"weight\": 1, \"max-fails\": 2, \"fail-timeout\": 30, \"ip\": \"10.2.6.201\"}, {\"port\": 8080, \"weight\": 2, \"max-fails\": 2, \"fail-timeout\": 30, \"ip\": \"10.2.6.202\"} ] }";
+        testApp = parseApp(appJsonData);
+        try {
+            insertedTestAppId = appRepo.add(testApp);
+            Assert.assertTrue(insertedTestAppId > 0);
+            for(int i = 0; i < 6; i++) {
+                String appJsonData1 = "{\"name\": \"testApp" + i + "\", \"app-id\": \"000000\", \"version\": 1, \"app-slbs\": [{\"slb-name\": \"default\", \"path\": \"/\", \"virtual-server\": {\"name\": \"testsite1\", \"ssl\": false, \"port\": \"80\", \"domains\": [{\"name\": \"tests1.ctrip.com\"} ] } } ], \"health-check\": {\"intervals\": 5000, \"fails\": 1, \"passes\": 1, \"uri\": \"/domaininfo/OnService.html\"}, \"load-balancing-method\": {\"type\": \"roundrobin\", \"value\": \"test\"}, \"app-servers\": [{\"port\": 8080, \"weight\": 1, \"max-fails\": 2, \"fail-timeout\": 30, \"ip\": \"10.2.6.201\"}, {\"port\": 8080, \"weight\": 2, \"max-fails\": 2, \"fail-timeout\": 30, \"ip\": \"10.2.6.202\"} ] }";
+                appRepo.add(parseApp(appJsonData1));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
     private void deleteApps() {
         String appName = "testApp";
@@ -194,17 +265,16 @@ public class ModelServiceTest extends AbstractSpringTest {
         }
     }
 
-    private void deleteSlb() {
-        String slbName = "default";
-        try {
-            Assert.assertEquals(1, slbRepo.delete(slbName));
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+    /********************* test end *********************/
+
+    @After
+    public void clearDb() {
+        deleteApps();
+        deleteSlb();
     }
 
     @AfterClass
-    public static void teardown() throws InterruptedException, ComponentLookupException, ComponentLifecycleException {
+    public static void tearDownDb() throws InterruptedException, ComponentLookupException, ComponentLifecycleException {
         mysqlDbServer.stop();
 
         DataSourceManager ds = ContainerLoader.getDefaultContainer().lookup(DataSourceManager.class);
