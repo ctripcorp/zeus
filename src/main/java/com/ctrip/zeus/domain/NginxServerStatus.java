666f0f09278e1d299a77c8b2373f6dfe6a61aa3a
mag <mag@ctrip.com>
2015-03-30 13:52:51 +0800

    Add nginx service implementation

---
+++ src/main/java/com/ctrip/zeus/domain/NginxServerStatus.java
@@ -0,0 +1,27 @@
+package com.ctrip.zeus.domain;
+
+import com.google.gson.Gson;
+
+/**
+ * User: mag
+ * Date: 3/24/2015
+ * Time: 1:32 PM
+ */
+public class NginxServerStatus {
+    private static enum Status{STARTED,STOPPED}
+
+    private String serverIP;
+    private Status status;
+    private int activeConnections;
+
+
+    public String toJson(){
+        Gson gson = new Gson();
+        return gson.toJson(this);
+    }
+
+    public static NginxServerStatus fromJson(String jsonStr){
+        Gson gson = new Gson();
+        return gson.fromJson(jsonStr,NginxServerStatus.class);
+    }
+}
--- src/main/java/com/ctrip/zeus/nginx/NginxOperator.java
+++ src/main/java/com/ctrip/zeus/nginx/NginxOperator.java
@@ -1,22 +1,96 @@
 package com.ctrip.zeus.nginx;
 
-import com.ctrip.zeus.model.entity.App;
-import com.ctrip.zeus.model.entity.Slb;
-import com.ctrip.zeus.model.entity.VirtualServer;
+import com.ctrip.zeus.domain.NginxResponse;
+import com.ctrip.zeus.domain.NginxServerStatus;
+import org.apache.commons.exec.CommandLine;
+import org.apache.commons.exec.DefaultExecutor;
+import org.apache.commons.exec.PumpStreamHandler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-import java.io.IOException;
+import java.io.*;
 
-/**
- * @author:xingchaowang
- * @date: 3/9/2015.
- */
-public interface NginxOperator {
+public class NginxOperator {
+    private static final Logger LOGGER = LoggerFactory.getLogger(NginxOperator.class);
 
-    void writeNginxConf(String path, String conf) throws IOException;
+    private static final String DEF_NGINX_CONF = "nginx.conf";
+    private static final String CONF_SUFFIX = ".conf";
+    private String nginxConfDir;
+    private String nginxBinDir;
 
-    void writeServerConf(String path, String conf) throws IOException;
 
-    void writeUpstreamsConf(String path, String conf) throws IOException;
+    public NginxOperator(String nginxConfDir, String nginxBinDir){
+        this.nginxConfDir = nginxConfDir;
+        this.nginxBinDir = nginxBinDir;
+    }
 
-    String reloadConf(String command);
+    public void writeNginxConf(String conf) throws IOException {
+        doWriteConf(nginxConfDir, DEF_NGINX_CONF,conf);
+    }
+
+    public void writeServerConf(String serverName, String conf) throws IOException {
+        String fileName =  serverName + CONF_SUFFIX;
+        doWriteConf(nginxConfDir + "/vhosts", fileName,conf);
+    }
+
+
+    public void writeUpstreamsConf(String upstreamName, String conf) throws IOException {
+        String fileName = upstreamName + CONF_SUFFIX;
+        doWriteConf(nginxConfDir + "/upstreams", fileName, conf);
+    }
+
+    public NginxResponse reloadConf() throws IOException{
+        try {
+            String command = nginxBinDir + "/nginx -s reload";
+            ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+            ByteArrayOutputStream errorStream = new ByteArrayOutputStream();
+            CommandLine commandline = CommandLine.parse(command);
+
+            DefaultExecutor exec = new DefaultExecutor();
+            exec.setExitValues(null);
+
+            PumpStreamHandler streamHandler = new PumpStreamHandler(outputStream,errorStream);
+            exec.setStreamHandler(streamHandler);
+
+            int exitVal = exec.execute(commandline);
+            boolean failed = exec.isFailure(exitVal);
+            String out = outputStream.toString("UTF-8");
+            String error = errorStream.toString("UTF-8");
+
+            NginxResponse response = new NginxResponse();
+            response.setOutMessage(out);
+            response.setErrMessage(error);
+            response.setSucceed(!failed);
+
+            return response;
+        } catch (IOException e) {
+            LOGGER.error("Fail to reload",e);
+            throw e;
+        }
+    }
+
+    public NginxServerStatus getRuntimeStatus() {
+        NginxServerStatus status = new NginxServerStatus();
+        return status;
+    }
+
+
+    private void doWriteConf(String confPath, String confFile, String confContent) throws IOException {
+        makeSurePathExist(confPath);
+        Writer writer = null;
+        try {
+            writer = new FileWriter(new File(confPath + File.separator + confFile));
+            writer.write(confContent);
+        } finally {
+            if (writer != null) {
+                writer.close();
+            }
+        }
+    }
+
+    private void makeSurePathExist(String confDir) {
+        File f = new File(confDir);
+        f.mkdirs();
+    }
+
 }
