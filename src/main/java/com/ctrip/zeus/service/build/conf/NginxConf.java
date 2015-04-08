86e4898da034790885d0560e83c7891c4f8e1946
fanqq <fanqq@Ctrip.com>
2015-04-08 14:05:03 +0800

    add ops requirements

--- src/main/java/com/ctrip/zeus/service/build/conf/NginxConf.java
+++ src/main/java/com/ctrip/zeus/service/build/conf/NginxConf.java
@@ -11,28 +11,40 @@ import com.netflix.config.DynamicPropertyFactory;
  */
 public class NginxConf {
     private static DynamicIntProperty nginxStatusPort = DynamicPropertyFactory.getInstance().getIntProperty("slb.nginx.status-port", 10001);
-    private static final int DEFAULT_WORKERS = 4;
+//    private static final int DEFAULT_WORKERS = 4;
 
     public static String generate(Slb slb) {
         StringBuilder b = new StringBuilder(1024);
 
-        Integer worker = slb.getNginxWorkerProcesses();
+//        Integer worker = slb.getNginxWorkerProcesses();
 
-        b.append("worker_processes ").append((worker==null||worker==0)?DEFAULT_WORKERS:worker).append(";\n");
+//        b.append("worker_processes ").append((worker==null||worker==0)?DEFAULT_WORKERS:worker).append(";\n");
+        b.append("worker_processes auto;\n")
+         .append("user nobody;\n")
+         .append("error_log /opt/logs/nginx/error.log;\n")
+         .append("worker_rlimit_nofile 65535;\n")
+         .append("pid logs/nginx.pid;\n");
 
-        b.append("events {").append("\n");
-        b.append("worker_connections 1024000;\n");
-//        b.append("use epoll; \n");//要求linux 内核 2.6+
-        b.append("}").append("\n");
 
-        b.append("http {").append("\n");
-        b.append("include    mime.types").append(";\n");
-        b.append("default_type    application/octet-stream").append(";\n");
-        b.append("keepalive_timeout    65").append(";\n");
+        b.append("events {\n")
+         .append("worker_connections 30720;\n")
+         .append("use epoll; \n")
+         .append("}\n");
+
+        b.append("http {\n");
+        b.append("include    mime.types;\n");
+        b.append("default_type    application/octet-stream;\n");
+        b.append("keepalive_timeout    65;\n");
+        b.append("log_format main '[$time_local] $host $hostname $server_addr $request_method \"$uri\" ' " +
+                "'\"$query_string\" $server_port $remote_user $remote_addr $http_x_forwarded_for ' " +
+                "'$server_protocol \"$http_user_agent\" \"$cookie_COOKIE\" \"$http_referer\" ' " +
+                "'$host $status $body_bytes_sent $request_time $upstream_response_time ' " +
+                "'$upstream_addr $upstream_status';\n");
+        b.append("access_log /opt/logs/nginx/access.log main;\n");
         b.append(statusConf());
-        b.append("include    upstreams/*.conf").append(";\n");
-        b.append("include    vhosts/*.conf").append(";\n");
-        b.append("}").append("\n");
+        b.append("include    upstreams/*.conf;\n");
+        b.append("include    vhosts/*.conf;\n");
+        b.append("}\n");
 
         return StringFormat.format(b.toString());
     }
