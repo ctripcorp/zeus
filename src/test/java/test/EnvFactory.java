d64bcee18538ef8cc5aaa2e4ec3768cfe71f0bea
fanqq <fanqq@Ctrip.com>
2015-06-02 16:04:28 +0800

    fix priority bug

--- src/test/java/test/EnvFactory.java
+++ src/test/java/test/EnvFactory.java
@@ -17,13 +17,14 @@ public class EnvFactory {
 //    private static final String vip="127.0.0.1";
     private static final String vip="10.2.27.21";
     private static final int vsNum=1;
-    private static final int groupNum=1;
+    private static final int groupNum=10;
     private static final String slbName="test-env";
     private static final String checkHealthPath="/checkHealth";
     private static final boolean isActivate=true;
     private static ReqClient reqClient = new ReqClient(host);
     private static String[] ipList = new String[]{"10.2.25.83"};
-    private static int[] portList = new int[]{20001,20002,20003,20004};
+//    private static int[] portList = new int[]{20001,20002,20003,20004};
+    private static int[] portList = new int[]{20001};
 
     private static List<VirtualServer> vsList = new ArrayList<>();
     private static List<Group> groupList = new ArrayList<>();
@@ -96,8 +97,8 @@ public class EnvFactory {
             grouptmp = new Group().setName("App_" + i).setAppId(String.valueOf(100000 + i)).setVersion(1).setHealthCheck(new HealthCheck().setFails(1)
                     .setIntervals(2000 * groupNum / 100).setPasses(1).setUri(checkHealthPath)).setLoadBalancingMethod(new LoadBalancingMethod().setType("roundrobin")
                     .setValue("test"))
-                    .addGroupSlb(new GroupSlb().setSlbId(slb.getId()).setSlbName(slbName).setPath("/App" + i).setRewrite("/App(01|02) /App$1?sleep=1&size=1")
-                            .setVirtualServer(vsList.get(i % vsNum)));
+                    .addGroupSlb(new GroupSlb().setSlbId(slb.getId()).setSlbName(slbName).setPath("/App" + i).setRewrite("/App(01|02)(abb.+|app.+) /App$1/$2?sleep=1&size=1")
+                            .setVirtualServer(vsList.get(i % vsNum)).setPriority(i));
             int tmp = i % portList.length;
             int portlength = portList.length;
             for ( int j = 0 ; j < ipList.length ; j++ )
