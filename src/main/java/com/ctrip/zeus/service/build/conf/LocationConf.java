a1869c15616471759235be1cf85b84e0647b1c55
fanqq <fanqq@Ctrip.com>
2015-06-11 14:49:06 +0800

    add rewrite empty check

--- src/main/java/com/ctrip/zeus/service/build/conf/LocationConf.java
+++ src/main/java/com/ctrip/zeus/service/build/conf/LocationConf.java
@@ -60,7 +60,7 @@ public class LocationConf {
     private static void addRewriteCommand(StringBuilder sb, Slb slb , VirtualServer vs , Group group) throws Exception {
         if (sb != null){
             String rewrite = getRewrite(slb,vs,group);
-            if (rewrite==null){
+            if (rewrite==null || rewrite.isEmpty()){
                 return;
             }
             String[] rewrites = rewrite.split(";");
