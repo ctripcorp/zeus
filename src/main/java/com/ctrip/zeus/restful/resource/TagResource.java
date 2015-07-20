cb259190663e26a290e47d3333b73879f6d2432c
Mengyi Zhou <bjrara@sina.com>
2015-07-20 18:38:00 +0800

    add resources

---
+++ src/main/java/com/ctrip/zeus/restful/resource/TagResource.java
@@ -0,0 +1,97 @@
+package com.ctrip.zeus.restful.resource;
+
+import com.ctrip.zeus.exceptions.ValidationException;
+import com.ctrip.zeus.restful.message.ResponseHandler;
+import com.ctrip.zeus.tag.TagBox;
+import com.ctrip.zeus.tag.entity.TagList;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.HttpHeaders;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.List;
+
+/**
+ * Created by zhoumy on 2015/7/16.
+ */
+@Component
+@Path("/")
+public class TagResource {
+    @Resource
+    private TagBox tagBox;
+    @Resource
+    private ResponseHandler responseHandler;
+
+    @GET
+    @Path("/tags")
+    @Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
+    public Response listTags(@Context HttpHeaders hh, @Context HttpServletRequest request) throws Exception {
+        List<String> list = tagBox.getAllTags();
+        TagList tagList = new TagList().setTotal(list.size());
+        for (String s : list) {
+            tagList.addTag(s);
+        }
+        return responseHandler.handle(tagList, hh.getMediaType());
+    }
+
+    @GET
+    @Path("/tag/rename")
+    @Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
+    public Response renameTag(@Context HttpHeaders hh,
+                              @Context HttpServletRequest request,
+                              @QueryParam("oldName") String oldName,
+                              @QueryParam("newName") String newName) throws Exception {
+        if (oldName == null || newName == null)
+            throw new ValidationException("Both oldName and newName must be provided.");
+        tagBox.renameTag(oldName, newName);
+        return responseHandler.handle(oldName + " is renamed to " + newName + ".", hh.getMediaType());
+    }
+
+    @GET
+    @Path("/tagging")
+    @Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
+    public Response tagging(@Context HttpHeaders hh,
+                            @Context HttpServletRequest request,
+                            @QueryParam("tagName") String tagName,
+                            @QueryParam("type") String type,
+                            @QueryParam("id") Long id) throws Exception {
+        if (tagName == null || type == null || id == null)
+            throw new ValidationException("At least one parameter is missing.");
+        tagBox.tagging(tagName, type, id);
+        return responseHandler.handle("Tagged " + id + " to " + tagName + ".", hh.getMediaType());
+    }
+
+    @GET
+    @Path("/untagging")
+    @Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
+    public Response untagging(@Context HttpHeaders hh,
+                              @Context HttpServletRequest request,
+                              @QueryParam("tagName") String tagName,
+                              @QueryParam("type") String type,
+                              @QueryParam("id") Long id,
+                              @QueryParam("batch") Boolean batch) throws Exception {
+        if (tagName == null)
+            throw new ValidationException("Tag name is required.");
+        if (type != null && id != null) {
+            tagBox.untagging(tagName, type, id);
+            return responseHandler.handle("Untagged " + id + " from " + tagName + ".", hh.getMediaType());
+        }
+        if (batch != null && batch.booleanValue()) {
+            if (type != null) {
+                tagBox.untagging(tagName, type, null);
+                return responseHandler.handle("Untagged all the items with typeName - " + type + " from " + tagName + ".", hh.getMediaType());
+            } else {
+                tagBox.removeTag(tagName);
+                return responseHandler.handle("Deleted tag named " + tagName + ".", hh.getMediaType());
+            }
+        }
+        throw new ValidationException("At least one parameter is missing.");
+    }
+}
