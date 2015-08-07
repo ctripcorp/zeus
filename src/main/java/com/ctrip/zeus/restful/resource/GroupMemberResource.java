0e21e187c62ff9e0a823cb7fc84d3444714ef534
Mengyi Zhou <bjrara@sina.com>
2015-08-07 11:33:40 +0800

    add server and member resource

---
+++ src/main/java/com/ctrip/zeus/restful/resource/GroupMemberResource.java
@@ -0,0 +1,100 @@
+package com.ctrip.zeus.restful.resource;
+
+import com.ctrip.zeus.auth.Authorize;
+import com.ctrip.zeus.exceptions.ValidationException;
+import com.ctrip.zeus.model.entity.GroupServer;
+import com.ctrip.zeus.model.entity.GroupServerList;
+import com.ctrip.zeus.model.entity.GroupVirtualServer;
+import com.ctrip.zeus.model.transform.DefaultJsonParser;
+import com.ctrip.zeus.model.transform.DefaultSaxParser;
+import com.ctrip.zeus.restful.message.ResponseHandler;
+import com.ctrip.zeus.service.model.GroupMemberRepository;
+import com.google.common.base.Joiner;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import javax.ws.rs.*;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.HttpHeaders;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.List;
+
+/**
+ * Created by zhoumy on 2015/8/6.
+ */
+@Component
+@Path("/")
+public class GroupMemberResource {
+    @Resource
+    private GroupMemberRepository groupMemberRepository;
+    @Resource
+    private ResponseHandler responseHandler;
+
+    @GET
+    @Path("/members")
+    @Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
+    @Authorize(name = "getAllByGroup")
+    public Response list(@Context HttpHeaders hh,
+                         @Context HttpServletRequest request,
+                         @QueryParam("groupId") Long groupId) throws Exception {
+        GroupServerList groupServerList = new GroupServerList();
+        for (GroupServer groupServer : groupMemberRepository.listGroupServersByGroup(groupId)) {
+            groupServerList.addGroupServer(groupServer);
+        }
+        return responseHandler.handle(groupServerList, hh.getMediaType());
+    }
+
+    @POST
+    @Path("/member/add")
+    @Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
+    @Authorize(name = "addMember")
+    public Response addMember(@Context HttpHeaders hh, @Context HttpServletRequest request, String groupServerList) throws Exception {
+        GroupServerList gsl = parseGroupServer(hh.getMediaType(), groupServerList);
+        for (GroupServer groupServer : gsl.getGroupServers()) {
+            groupMemberRepository.addGroupServer(gsl.getGroupId(), groupServer);
+        }
+        return responseHandler.handle("Successfully added group servers to group with id " + gsl.getGroupId() + ".", hh.getMediaType());
+    }
+
+    @POST
+    @Path("/member/update")
+    @Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
+    @Authorize(name = "updateMember")
+    public Response updateMember(@Context HttpHeaders hh, @Context HttpServletRequest request, String groupServerList) throws Exception {
+        GroupServerList gsl = parseGroupServer(hh.getMediaType(), groupServerList);
+        for (GroupServer groupServer : gsl.getGroupServers()) {
+            groupMemberRepository.updateGroupServer(gsl.getGroupId(), groupServer);
+        }
+        return responseHandler.handle("Successfully updated group servers to group with id " + gsl.getGroupId() + ".", hh.getMediaType());
+    }
+
+    @GET
+    @Path("/member/remove")
+    @Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
+    @Authorize(name = "removeMember")
+    public Response removeMember(@Context HttpHeaders hh, @Context HttpServletRequest request, @QueryParam("groupId") Long groupId,
+                                 @QueryParam("ip") List<String> ips) throws Exception {
+        if (groupId == null)
+            throw new ValidationException("No groupId is provided.");
+        for (String ip : ips) {
+            groupMemberRepository.removeGroupServer(groupId, ip);
+        }
+        return responseHandler.handle("Successfully removed " + Joiner.on(",").join(ips) + " from group with id " + groupId + ".", hh.getMediaType());
+    }
+    
+    private GroupServerList parseGroupServer(MediaType mediaType, String groupServerList) throws Exception {
+        GroupServerList gsl;
+        if (mediaType.equals(MediaType.APPLICATION_XML_TYPE)) {
+            gsl = DefaultSaxParser.parseEntity(GroupServerList.class, groupServerList);
+        } else {
+            try {
+                gsl = DefaultJsonParser.parse(GroupServerList.class, groupServerList);
+            } catch (Exception e) {
+                throw new Exception("Group cannot be parsed.");
+            }
+        }
+        return gsl;
+    }
+}
---
+++ src/main/java/com/ctrip/zeus/restful/resource/VirtualServerResource.java
@@ -0,0 +1,109 @@
+package com.ctrip.zeus.restful.resource;
+
+import com.ctrip.zeus.auth.Authorize;
+import com.ctrip.zeus.exceptions.ValidationException;
+import com.ctrip.zeus.model.entity.VirtualServer;
+import com.ctrip.zeus.model.entity.VirtualServerList;
+import com.ctrip.zeus.model.transform.DefaultJsonParser;
+import com.ctrip.zeus.model.transform.DefaultSaxParser;
+import com.ctrip.zeus.restful.message.ResponseHandler;
+import com.ctrip.zeus.service.model.VirtualServerRepository;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import javax.ws.rs.*;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.HttpHeaders;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+/**
+ * Created by zhoumy on 2015/8/5.
+ */
+@Component
+@Path("/")
+public class VirtualServerResource {
+    @Resource
+    private VirtualServerRepository virtualServerRepository;
+    @Resource
+    private ResponseHandler responseHandler;
+
+    @GET
+    @Path("/vses")
+    @Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
+    @Authorize(name = "getAllVses")
+    public Response list(@Context HttpHeaders hh,
+                         @Context HttpServletRequest request) throws Exception {
+        VirtualServerList vslist = new VirtualServerList();
+        for (VirtualServer virtualServer : virtualServerRepository.listAll()) {
+            vslist.addVirtualServer(virtualServer);
+        }
+        return responseHandler.handle(virtualServerRepository, hh.getMediaType());
+    }
+
+    @GET
+    @Path("/vs")
+    @Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
+    @Authorize(name = "getVs")
+    public Response getVirtualServer(@Context HttpHeaders hh,
+                                     @Context HttpServletRequest request,
+                                     @QueryParam("vsId") Long vsId) throws Exception {
+        VirtualServer vs = virtualServerRepository.getById(vsId);
+        return responseHandler.handle(vs, hh.getMediaType());
+    }
+
+    @POST
+    @Path("/vs/new")
+    @Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
+    @Authorize(name = "addVs")
+    public Response addVirtualServer(@Context HttpHeaders hh,
+                                     @Context HttpServletRequest request, String vs) throws Exception {
+        VirtualServer virtualServer = parseVirtualServer(hh.getMediaType(), vs);
+        if (virtualServer.getSlbId() == null)
+            throw new ValidationException("Slb id is not provided.");
+        virtualServer = virtualServerRepository.addVirtualServer(virtualServer.getSlbId(), virtualServer);
+        return responseHandler.handle(virtualServer, hh.getMediaType());
+
+    }
+
+    @POST
+    @Path("/vs/update")
+    @Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
+    @Authorize(name = "updateVs")
+    public Response updateVirtualServer(@Context HttpHeaders hh,
+                                        @Context HttpServletRequest request, String vs) throws Exception {
+        VirtualServer virtualServer = parseVirtualServer(hh.getMediaType(), vs);
+        if (virtualServer.getSlbId() == null)
+            throw new ValidationException("Slb id is not provided.");
+        virtualServerRepository.updateVirtualServers(new VirtualServer[]{virtualServer});
+        return responseHandler.handle(virtualServer, hh.getMediaType());
+    }
+
+    @GET
+    @Path("/vs/delete")
+    @Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
+    @Authorize(name = "deleteVs")
+    public Response deleteVirtualServer(@Context HttpHeaders hh,
+                                        @Context HttpServletRequest request,
+                                        @QueryParam("vsId") Long vsId) throws Exception {
+        if (vsId == null)
+            throw new ValidationException("vsId is required.");
+        virtualServerRepository.deleteVirtualServer(vsId);
+        return responseHandler.handle("Successfully deleted virtual server with id " + vsId + ".", hh.getMediaType());
+    }
+
+    private VirtualServer parseVirtualServer(MediaType mediaType, String virtualServer) throws Exception {
+        VirtualServer vs;
+        if (mediaType.equals(MediaType.APPLICATION_XML_TYPE)) {
+            vs = DefaultSaxParser.parseEntity(VirtualServer.class, virtualServer);
+        } else {
+            try {
+                vs = DefaultJsonParser.parse(VirtualServer.class, virtualServer);
+            } catch (Exception e) {
+                throw new Exception("Group cannot be parsed.");
+            }
+        }
+        return vs;
+    }
+}
