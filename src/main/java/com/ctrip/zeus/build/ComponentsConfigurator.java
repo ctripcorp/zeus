7d461ce8bf8078393313496967394c698b277cbf
Mengyi Zhou <bjrara@sina.com>
2015-12-07 19:30:14 +0800

    upgrade dal

--- src/main/java/com/ctrip/zeus/build/ComponentsConfigurator.java
+++ src/main/java/com/ctrip/zeus/build/ComponentsConfigurator.java
@@ -3,26 +3,26 @@ package com.ctrip.zeus.build;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.unidal.dal.jdbc.datasource.JdbcDataSourceDescriptorManager;
+import org.unidal.dal.jdbc.datasource.DataSourceProvider;
+import org.unidal.dal.jdbc.datasource.DefaultDataSourceProvider;
 import org.unidal.lookup.configuration.AbstractResourceConfigurator;
 import org.unidal.lookup.configuration.Component;
+import org.unidal.lookup.configuration.Configuration;
 
 public class ComponentsConfigurator extends AbstractResourceConfigurator {
-   @Override
-   public List<Component> defineComponents() {
-      List<Component> all = new ArrayList<Component>();
+    @Override
+    public List<Component> defineComponents() {
+        List<Component> all = new ArrayList<Component>();
+        all.add(C(DataSourceProvider.class, DefaultDataSourceProvider.class)
+                .config(new Configuration[]{
+                        E("datasourceFile", new String[0]).value("datasources.xml"),
+                        E("baseDirRef", new String[0]).value("CONF_DIR")
+                }));
+        all.addAll(new ZeusDatabaseConfigurator().defineComponents());
+        return all;
+    }
 
-      // move following line to top-level project if necessary
-		all.add(C(JdbcDataSourceDescriptorManager.class) //
-				.config(E("datasourceFile").value("datasources.xml"), //
-						E("baseDirRef").value("CONF_DIR")));
-
-      all.addAll(new ZeusDatabaseConfigurator().defineComponents());
-
-      return all;
-   }
-
-   public static void main(String[] args) {
-      generatePlexusComponentsXmlFile(new ComponentsConfigurator());
-   }
+    public static void main(String[] args) {
+        generatePlexusComponentsXmlFile(new ComponentsConfigurator());
+    }
 }
