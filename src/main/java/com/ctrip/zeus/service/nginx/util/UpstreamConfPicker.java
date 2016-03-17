0913e0159bbe59703e7efe01959eca37dc3e8d42
Mengyi Zhou <zhoumy@ctrip.com>
2016-03-17 10:46:40 +0800

    add class prototype

---
+++ src/main/java/com/ctrip/zeus/service/nginx/util/UpstreamConfPicker.java
@@ -0,0 +1,17 @@
+package com.ctrip.zeus.service.nginx.util;
+
+import com.ctrip.zeus.model.entity.DyUpstreamOpsData;
+import com.ctrip.zeus.nginx.entity.VsConfData;
+
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * Created by zhoumy on 2016/3/16.
+ */
+public class UpstreamConfPicker {
+
+    public static DyUpstreamOpsData[] pickByGroupIds(Map<Long, VsConfData> vsConf, Set<Long> groupIds) throws Exception {
+
+    }
+}
