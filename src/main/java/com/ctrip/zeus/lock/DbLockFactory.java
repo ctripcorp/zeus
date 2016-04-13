e21ad5854fac17ce92f32722a0c626ab5673ae1c
Mengyi Zhou <zhoumy@ctrip.com>
2016-04-13 19:14:58 +0800

    release dead lock after spring inited

--- src/main/java/com/ctrip/zeus/lock/DbLockFactory.java
+++ src/main/java/com/ctrip/zeus/lock/DbLockFactory.java
@@ -1,16 +1,18 @@
 package com.ctrip.zeus.lock;
 
 import com.ctrip.zeus.dal.core.DistLockDao;
+import com.ctrip.zeus.dal.core.DistLockDo;
+import com.ctrip.zeus.dal.core.DistLockEntity;
 import com.ctrip.zeus.lock.impl.MysqlDistLock;
+import com.ctrip.zeus.util.S;
 import com.netflix.config.DynamicPropertyFactory;
 import com.netflix.config.DynamicStringProperty;
-import org.springframework.beans.BeansException;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.ApplicationContextAware;
 import org.springframework.stereotype.Component;
 import org.unidal.dal.jdbc.transaction.TransactionManager;
 
+import javax.annotation.PostConstruct;
 import javax.annotation.Resource;
+import java.util.List;
 
 /**
  * Created by zhoumy on 2015/4/23.
@@ -38,5 +40,14 @@ public class DbLockFactory {
 
     public String getResourceName() {
         return dbName.get();
+    }
+
+    @PostConstruct
+    public void releaseDeadLocks() throws Exception {
+        List<DistLockDo> check = distLockDao.getByServer(S.getIp(), DistLockEntity.READSET_FULL);
+        for (DistLockDo d : check) {
+            d.setServer("").setOwner(0L).setCreatedTime(System.currentTimeMillis());
+        }
+        distLockDao.updateByKey(check.toArray(new DistLockDo[check.size()]), DistLockEntity.UPDATESET_FULL);
     }
 }
