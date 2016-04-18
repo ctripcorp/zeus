183c7b0e4504fa1e76c49a72f844fea2c3995bb4
fanqq <fanqq@ctrip.com>
2016-04-18 18:34:00 +0800

    fix CleanDbTask bug

--- src/main/java/com/ctrip/zeus/task/clean/CleanDbTask.java
+++ src/main/java/com/ctrip/zeus/task/clean/CleanDbTask.java
@@ -4,12 +4,14 @@ import com.ctrip.zeus.clean.CleanDbManager;
 import com.ctrip.zeus.task.AbstractTask;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
 
 import javax.annotation.Resource;
 
 /**
  * Created by fanqq on 2016/1/5.
  */
+@Component("cleanDbTask")
 public class CleanDbTask extends AbstractTask {
     Logger logger = LoggerFactory.getLogger(this.getClass());
     @Resource
@@ -23,7 +25,9 @@ public class CleanDbTask extends AbstractTask {
     @Override
     public void run() throws Exception {
         try {
+            logger.info("[CleanDbTask] clean db task started.");
             cleanDbManager.run();
+            logger.info("[CleanDbTask] clean db task finished.");
         } catch (Exception e) {
             logger.warn("[clean db job] clean db exception." + e.getMessage(), e);
         }
